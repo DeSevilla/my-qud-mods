694ffdc941e2fd5067e8754f420229b96e0a2cc1
DeSevilla <isidoremohr@gmail.com>
2023-09-25 18:41:41 -0400

    Now have multiple mods in their own collections

---
+++ monsters/IlluminatiSwag_Monsters_Terrifying.cs
@@ -0,0 +1,40 @@
+using System;
+// using XRL;
+// using XRL.World;
+using XRL.World.Effects;
+using XRL.World.Capabilities;
+
+namespace XRL.World.Parts {
+
+    [Serializable]
+    public class IlluminatiSwag_Monsters_Terrifying : IPart
+    {
+        public override void Register(GameObject Object)
+        {
+            Object.RegisterPartEvent(this, "AfterLookedAt");
+            base.Register(Object);
+        }
+
+        public override bool FireEvent(Event E)
+        {
+            if (E.ID == "AfterLookedAt")
+            {
+                GameObject looker = E.GetGameObjectParameter("Looker");
+                if (looker != null && !looker.HasEffect("Terrified"))
+                {
+                    int attackModifier = ParentObject.StatMod("Ego") + ParentObject.GetIntProperty("Persuasion_Intimidate") * 2;
+                    Mental.PerformAttack(
+                        Handler: Terrified.OfAttacker,
+                        Attacker: ParentObject,
+                        Defender: looker, 
+                        Command: "Terrify Intimidate",
+                        Dice: "4d10",
+                        Type: 8388610,
+                        Magnitude: "2d8".RollCached(),
+                        AttackModifier: attackModifier);
+                }
+            }
+            return base.FireEvent(E);
+        }
+    }
+}
